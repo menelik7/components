029e3508687721e669667fbb7fcfdd89db4048b2
menelik7 <menelikworku@gmail.com>
2023-06-09 15:09:27 +0200

    Initial commit on Accordion component with tailwind css

---
+++ src/components/Accordion.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { GoChevronRight } from "react-icons/go";
+
+export default function Accordion({ items }) {
+	const [expandedIndex, setExpandedIndex] = useState(-1);
+
+	const handleClick = (clickedIndex) => {
+		if (clickedIndex === expandedIndex) {
+			setExpandedIndex(-1);
+			return;
+		}
+		setExpandedIndex(clickedIndex);
+	};
+
+	const renderedItems = items.map((item, index) => {
+		const isExpanded = index === expandedIndex;
+
+		return (
+			<div key={item.id}>
+				<div
+					className="flex justify-between items-center p-3 bg-gray-50 border-b cursor-pointer"
+					onClick={() => handleClick(index)}
+				>
+					<div>{item.label}</div>
+					<div className={`text-2xl ${isExpanded ? "expanded" : "collapsed"}`}>
+						<GoChevronRight />
+					</div>
+				</div>
+				{isExpanded && (
+					<div className="border-b p-5 active">{item.content}</div>
+				)}
+			</div>
+		);
+	});
+
+	return <div className="border-x border-t rounded">{renderedItems}</div>;
+}
