e070047755fe7dfd3991e685c72ef1b646c30860
menelik7 <menelikworku@gmail.com>
2023-06-12 14:22:27 +0200

    Refactoring dropdown to use a panel component to eliminate duplicate classnames as well as have consistency across the app

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Panel from "./Panel";
+import { GoChevronRight } from "react-icons/go";
 
 export default function Dropdown({ options, value, onChange }) {
 	const [isOpen, setIsOpen] = useState(false);
@@ -14,18 +16,29 @@ export default function Dropdown({ options, value, onChange }) {
 
 	const renderedOptions = options.map((option) => {
 		return (
-			<div onClick={() => handleOptionclick(option)} key={option.value}>
+			<div
+				className="hover:bg-sky-100 rounded cursor-pointer p-1"
+				onClick={() => handleOptionclick(option)}
+				key={option.value}
+			>
 				{option.label}
 			</div>
 		);
 	});
 
 	return (
-		<div>
-			<div className="cursor-pointer" onClick={handleClick}>
+		<div className="w-48 relative">
+			<Panel
+				className="flex justify-between items-center cursor-pointer"
+				onClick={handleClick}
+			>
 				{value?.label || "Select..."}
-			</div>
-			{isOpen && <div>{renderedOptions}</div>}
+				<div className={`text-xl ${isOpen ? "expanded" : "collapsed"}`}>
+					<GoChevronRight />
+				</div>
+			</Panel>
+
+			{isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
 		</div>
 	);
 }
